41e906980da1767ffa7f2ac35f8f4f55a58b9e12
Aditya Venugopal <aditya.venugopal@happiestminds.com>
2019-06-06 14:13:17 +0530

    precommit move to dev dependencies

--- src/app/Header.js
+++ src/app/Header.js
@@ -1,39 +1,38 @@
-import React from 'react'
-import {NavLink, Link} from 'react-router-dom'
-import Button from '../components/Button'
-import images from '../assets/images'
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import Button from '../components/Button';
+import images from '../assets/images';
 
-const Header = () => (<header id="header">
-  <div className="content header-wrap">
- <section>
-      <Link to="/home" id="header__branding">
-        <img src={images.logo} alt="header logo" />
-        <span className="brand-text">CRUD</span>
-      </Link>
-    </section>
-    <nav>
-      <NavLink
-        to={'/users'}
-        className="nav-link"
-        activeClassName="nav-link--active">
-            Users
-      </NavLink>
-      <NavLink
-        to={'/projects'}
-        className="nav-link"
-        activeClassName="nav-link--active">
+const Header = () => (
+  <header id="header">
+    <div className="content header-wrap">
+      <section>
+        <Link to="/home" id="header__branding">
+          <img src={images.logo} alt="header logo" />
+          <span className="brand-text">CRUD</span>
+        </Link>
+      </section>
+      <nav>
+        <NavLink
+          to={'/users'}
+          className="nav-link"
+          activeClassName="nav-link--active"
+        >
+          Users
+        </NavLink>
+        <NavLink
+          to={'/projects'}
+          className="nav-link"
+          activeClassName="nav-link--active"
+        >
           Projects
-      </NavLink>
-    </nav>
-    <section>
-      <Button
-        type="button"
-        icon="user-circle"
-        iconSize="3x"
-        iconButton
-      />
-    </section>
-  </div>
-</header>)
+        </NavLink>
+      </nav>
+      <section>
+        <Button type="button" icon="user-circle" iconSize="3x" iconButton />
+      </section>
+    </div>
+  </header>
+);
 
-export default Header
+export default Header;
